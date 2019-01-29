b23ef895f214a2645d007aec72ca72f9705c45c1
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2019-01-29 07:22:23 -0500

    (add) Initial commit of EDA notebook and dataset setup script.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# DS440
+# DS440 Capstone
+
+## Setup Routines
+To initialize this repository, first, setup the dataset folder by running the following bash script:
+```
+./setup.sh
+```
+
+Additional files, namely the following files below, must be manually downloaded and added into the directory: `data/raw/misc/`:
+```
+DR25_DEModel_NoisyTargetList.txt
+q1q17_dr25_gaia_fgk.csv
+```
+
+The files above can be downloaded from the following Box folder: https://psu.app.box.com/v/ds400spring2019planets
+
+## Useful Repositories
+Below are repositories found on Github which may be of use during our project.
+* https://github.com/eford/DS400Spring2019Planets
+* https://github.com/nasa/kepler-robovetter
+* https://github.com/dfm/dr25
+* https://github.com/JeffLCoughlin/skye
+* https://github.com/PsuAstro528
---
+++ setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Dataset Environment Setup Script
+# Author: Yuya Jeremy Ong (yjo5006@psu.edu)
+
+# Source: https://github.com/dfm/dr25/blob/master/get_data.sh
+
+echo "[Dataset Environment Setup]\n"
+
+# Create Directories
+echo "> Generating Directories"
+mkdir -p data
+mkdir -p data/raw/
+mkdir -p data/preproc/
+
+# Download KPLR DR25 PLTI Injected Dataset
+echo "> Downloading: KPLR DR25 PLTI Injected Dataset"
+mkdir -p data/raw/plti/
+wget -O data/raw/plti/kplr_dr25_inj1_plti.txt https://exoplanetarchive.ipac.caltech.edu/data/KeplerData/Simulated/kplr_dr25_inj1_plti.txt
+wget -O data/raw/plti/kplr_dr25_inj2_plti.txt https://exoplanetarchive.ipac.caltech.edu/data/KeplerData/Simulated/kplr_dr25_inj2_plti.txt
+wget -O data/raw/plti/kplr_dr25_inj3_plti.txt https://exoplanetarchive.ipac.caltech.edu/data/KeplerData/Simulated/kplr_dr25_inj3_plti.txt
+
+# Download KPLR DR25 TCES Injected Dataset
+echo "> Downloading: KPLR DR25 TCES Injected Dataset"
+mkdir -p data/raw/tces/
+wget -O data/raw/tces/kplr_dr25_inj1_tces.txt https://exoplanetarchive.ipac.caltech.edu/data/KeplerData/Simulated/kplr_dr25_inj1_tces.txt
+wget -O data/raw/tces/kplr_dr25_inj2_tces.txt https://exoplanetarchive.ipac.caltech.edu/data/KeplerData/Simulated/kplr_dr25_inj2_tces.txt
+wget -O data/raw/tces/kplr_dr25_inj3_tces.txt https://exoplanetarchive.ipac.caltech.edu/data/KeplerData/Simulated/kplr_dr25_inj3_tces.txt
+
+# Download KPLR DR25 Robovetter Injection Dataset
+echo "> Downloading: KPLR DR25 Robovetter Injection Dataset"
+git clone https://github.com/nasa/kepler-robovetter.git
+
+# Build Robovetter Raw Data Directories
+mkdir -p data/raw/robovetter/
+mkdir -p data/raw/robovetter/inj
+mkdir -p data/raw/robovetter/inv
+mkdir -p data/raw/robovetter/obs
+mkdir -p data/raw/robovetter/scr
+mkdir -p data/raw/robovetter/sup
+
+# Rearrange File Structure from Git Repository
+mv kepler-robovetter/kplr_dr25_inj* data/raw/robovetter/inj
+mv kepler-robovetter/kplr_dr25_inv* data/raw/robovetter/inv
+mv kepler-robovetter/kplr_dr25_obs* data/raw/robovetter/obs
+mv kepler-robovetter/kplr_dr25_scr* data/raw/robovetter/scr
+mv kepler-robovetter/kplr_dr25_sup* data/raw/robovetter/sup
+
+# Cleanup Directories
+rm -rf kepler-robovetter
+
+# Obtain Stellar and KOI Dataset
+echo "> Download Stellar and KOI Dataset"
+mkdir -p data/raw/misc/
+wget -O data/raw/misc/q1_q17_dr25_stellar.txt "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=q1_q17_dr25_stellar&format=ipac&select=*"
+wget -O data/raw/misc/q1_q17_dr25_koi.txt "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=q1_q17_dr25_koi&format=ipac&select=*"
+
+echo "> Done"
